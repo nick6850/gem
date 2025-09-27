78e24246ce5c25000991d06f4e9d530df478f887
Nick Matyushin <n.matyushin@comagic.dev>
2025-09-28 02:55:57 +0300

    Improved example button

--- content.js
+++ content.js
@@ -736,8 +736,8 @@ contextButton.addEventListener("click", createQuickPromptCallback(
 
 exampleButton.addEventListener("click", createQuickPromptCallback(
   "Use it in a sentence",
-  "Use it in a sentence",
-  "Use it in a sentence"
+  "Use it in another unrelated sentence with the same meaning.",
+  "Use it in another unrelated sentence with the same meaning."
 ));
 
 shortifyButton.addEventListener("click", createQuickPromptCallback(
